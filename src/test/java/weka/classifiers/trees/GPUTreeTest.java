cfc66201d7cf3a8183ef286b1f08690fb740d7e2
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2016-05-16 18:47:21 +1200

    Added benchmarks/ integration tests against REPTree

--- src/test/java/weka/classifiers/trees/GPUTreeTest.java
+++ src/test/java/weka/classifiers/trees/GPUTreeTest.java
@@ -1,21 +1,3 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- * Copyright 2002 University of Waikato
- */
 
 package weka.classifiers.trees;
 
@@ -24,6 +6,10 @@ import weka.classifiers.Classifier;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.datagenerators.DataGenerator;
+import weka.datagenerators.classifiers.classification.RDG1;
 
 /**
  * Tests GPUTree. Run from the command line with:<p>
@@ -39,12 +25,96 @@ public class GPUTreeTest extends AbstractClassifierTest {
         return new GPUTree();
     }
 
+
+    public static void benchmark() throws Exception {
+
+        int nExamples = 1000;
+
+        //Number of timing runs to average
+        final int testIterations = 10;
+        //Number runs to warm up JIT compiler
+        final int hotspotIterations = 2;
+
+        final int nAttributes = 10;
+        final int maxLevels = 4;
+
+        System.out.println("Levels, Instances, GPUTree, REPTree, speedup");
+
+        for (int i = 0; i < 8; i++) {
+
+            //Generate random instances
+            RDG1 generator = new RDG1();
+            generator.setMaxRuleSize(50);
+            generator.setNumAttributes(nAttributes);
+            generator.setNumNumeric(nAttributes);
+            generator.setNumExamples(nExamples);
+            generator.defineDataFormat();
+            Instances instances = generator.generateExamples();
+
+
+            GPUTree gpuTree = new GPUTree();
+            long gpuElapsed = 0;
+            for (int j = 0; j < testIterations + hotspotIterations; j++) {
+
+                gpuTree = new GPUTree();
+                gpuTree.setMaxDepth(maxLevels);
+                gpuTree.preExecution();
+                long start = System.currentTimeMillis();
+                gpuTree.buildClassifier(instances);
+                if (j > hotspotIterations) {
+                    gpuElapsed += System.currentTimeMillis() - start;
+                }
+            }
+            double gpuSeconds = (double) gpuElapsed / 1000 / testIterations;
+            System.out.print(maxLevels + ", ");
+            System.out.print(nExamples + ", ");
+            System.out.print(String.format("%1.2f", gpuSeconds) + ", ");
+
+
+            REPTree repTree = new REPTree();
+            long repElapsed = 0;
+            for (int j = 0; j < testIterations + hotspotIterations; j++) {
+                repTree = new REPTree();
+                repTree.setMaxDepth(maxLevels);
+                repTree.setNoPruning(true);
+                repTree.setMinNum(0);
+                long start = System.currentTimeMillis();
+                repTree.buildClassifier(instances);
+                if (j > hotspotIterations) {
+                    repElapsed += System.currentTimeMillis() - start;
+                }
+            }
+
+            double repSeconds = (double) repElapsed / 1000 / testIterations;
+            System.out.print(String.format("%1.2f", repSeconds) + ", ");
+
+            System.out.print(String.format("%1.2f", repSeconds / gpuSeconds) + "\n");
+
+            //Check correctness
+            for (Instance inst : instances) {
+                if (gpuTree.classifyInstance(inst) != repTree.classifyInstance(inst)) {
+                    System.out.println("GPUTree prediction: " + gpuTree.classifyInstance(inst));
+                    System.out.println("REPTree prediction: " + repTree.classifyInstance(inst));
+                    System.out.println("GPUTree tree:");
+                    System.out.println(gpuTree.toString());
+                    System.out.println("REPTree tree:");
+                    System.out.println(repTree.toString());
+                    System.exit(-1);
+                }
+            }
+
+            nExamples *= 2;
+        }
+    }
+
     public static Test suite() {
         return new TestSuite(GPUTreeTest.class);
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
         junit.textui.TestRunner.run(suite());
+        benchmark();
     }
 
 }
